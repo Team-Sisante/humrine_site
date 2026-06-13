21250fef48b79a35cebc5e48ecaf5d9950817222
xmione <solomiosisante@gmail.com>
2026-06-13 14:30:22 +0800

    Implement affiliate stats page with total clicks and breakdowns by link and day

--- affiliate/views.py
+++ affiliate/views.py
@@ -1,7 +1,14 @@
 # affiliate/views.py
 
-from django.shortcuts import get_object_or_404, redirect
+from datetime import date
+
+from django.contrib.admin.views.decorators import staff_member_required
+from django.db.models import Count
+from django.db.models.functions import TruncDate
+from django.shortcuts import get_object_or_404, redirect, render
+
 from .models import TrackedAffiliateLink, AffiliateClick
+
 
 def affiliate_redirect(request, slug):
     link = get_object_or_404(TrackedAffiliateLink, slug=slug)
@@ -16,3 +23,28 @@ def affiliate_redirect(request, slug):
     )
 
     return redirect(link.original_url)
+
+
+@staff_member_required
+def affiliate_stats(request):
+    # Total clicks per link
+    clicks_per_link = TrackedAffiliateLink.objects.annotate(
+        click_count=Count('clicks')
+    ).order_by('-click_count')
+
+    # Clicks per day (last 30 days)
+    clicks_per_day = (
+        AffiliateClick.objects
+        .annotate(day=TruncDate('clicked_at'))
+        .values('day')
+        .annotate(count=Count('id'))
+        .order_by('-day')[:30]
+    )
+
+    context = {
+        'clicks_per_link': clicks_per_link,
+        'clicks_per_day': clicks_per_day,
+        'total_clicks': AffiliateClick.objects.count(),
+        'today': date.today(),
+    }
+    return render(request, 'affiliate/stats.html', context)
