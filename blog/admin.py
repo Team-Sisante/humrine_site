36b47dd7f2d92a8eef92fe6e8396ee0477fc494a
xmione <solomiosisante@gmail.com>
2026-06-25 22:01:09 +0800

    Add PostImage model and update admin and templates for image handling

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, PostImage
+
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    extra = 1
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'published']
     prepopulated_fields = {'slug': ('title',)}
+    inlines = [PostImageInline]    
--- blog/models.py
+++ blog/models.py
@@ -19,3 +19,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='blog/')
+    caption = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f"Image for {self.post.title}"    
+
+    
