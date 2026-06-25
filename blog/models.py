d2f64f96383849d09fb5dc758b7176a9ada6349b
xmione <solomiosisante@gmail.com>
2026-06-25 13:30:41 +0800

    Add blog feature with models, views, and templates; update URLs and base template

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, max_length=200, blank=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='blog/', blank=True, null=True)
+    published = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('blog_detail', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return self.title
