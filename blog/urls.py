d6e05564776cae7c90ad964ee2ae2856d58cc7c9
xmione <solomiosisante@gmail.com>
2026-06-26 10:47:32 +0800

    feat: add CKEditor URL configuration to main project URLs

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,4 @@ from . import views
 urlpatterns = [
     path('', views.PostListView.as_view(), name='blog_list'),
     path('<slug:slug>/', views.PostDetailView.as_view(), name='blog_detail'),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
